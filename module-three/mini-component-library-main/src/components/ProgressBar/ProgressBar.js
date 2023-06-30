d72cfff6ecb55801b6f53599d42777b160d31e9b
Andrei Seandru <dunkradu1234@gmail.com>
2023-06-30 12:48:17 +0300

    add progress bar div

--- module-three/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
+++ module-three/mini-component-library-main/src/components/ProgressBar/ProgressBar.js
@@ -6,7 +6,16 @@ import { COLORS } from '../../constants';
 import VisuallyHidden from '../VisuallyHidden';
 
 const ProgressBar = ({ value, size }) => {
-  return <strong>{value}</strong>;
+  return (
+  <div
+    role='progressbar'
+    aria-valuenow={value}
+    aria-valuemin='0'
+    aria-valuemax='100'
+  >
+    
+  </div>
+  );
 };
 
 export default ProgressBar;
